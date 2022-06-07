bb30c3126eb50bccc501da602c438baf1e07edf0
reinhaaard13 <reinhardkevin.rk@gmail.com>
2022-06-07 17:43:26 +0700

    headless puppeteer

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const qrcode = require("qrcode-terminal");
 
 const client = new Client({
 	authStrategy: new LocalAuth(),
+  puppeteer: {
+    headless: false,
+    args: ["--no-sandbox", "--disable-setuid-sandbox"]
+  }
 });
 
 // Express server
